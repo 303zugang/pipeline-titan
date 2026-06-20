0209ba55321692c2a124e400d5082f6475b5900c
303zugang <jkellypeterson@gmail.com>
2026-06-20 13:51:05 -0600

    Add files via upload
    add CORS for pipelinetitan.com fetch requests

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const { pool, initDb } = require('./db');
 const { buildSystem, inboundPrompt, followupPrompt, replyPrompt, ACTIVE_PROFILE } = require('./prompts');
 
 const app = express();
+
+// Allow fetch() from pipelinetitan.com to reach this API
+app.use((req, res, next) => {
+  const allowed = ['https://pipelinetitan.com', 'https://www.pipelinetitan.com'];
+  if (allowed.includes(req.headers.origin)) {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-Api-Key');
+  }
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
